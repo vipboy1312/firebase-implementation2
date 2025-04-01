54bfb622469be08a289fa538a7e6139658f76c57
Nguyen Trong Quy <trongquy131204@gmail.com>
2025-04-01 16:29:42 +0700

    fourth commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Auth from "./pages/Auth";
-import Home from "./pages/Home";
 function App() {
   return (
     <Routes>
